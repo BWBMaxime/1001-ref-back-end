d736e5a412acb34bb20af8698b82e5f037ea2bd3
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-09-13 10:09:38 +0000

    get user and serializer

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,7 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
-use Serializable;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class AccountController extends AbstractController
 { 
@@ -123,6 +127,21 @@ class AccountController extends AbstractController
     }
 
     /**
+     * @Route("/user/get/{id}", name="getUser", methods={"GET"})
+     */
+    public function getUser(int $id): Response
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $result = $this->getSerializer()->serialize($user, 'json');
+
+        return new Response(
+            $result,
+            Response::HTTP_OK,
+            ['Access-Control-Allow-Origin' => '*']
+        );
+    }
+
+    /**
      * @Route("/getCred", name="getCred", methods={"POST"})
      */
     public function getCredentials(Request $request): Response
@@ -152,5 +171,16 @@ class AccountController extends AbstractController
             );
         }        
         return $response;
+    }
+
+    private function getSerializer(){
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+            return $object->getId();
+        },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        return new Serializer([$normalizer], [$encoder]);
     }
 }
